9ba13670ae611774008237d9ef6634bcc464794f
/u/leo60228 <leo60228@users.noreply.github.com>
2017-07-12 16:48:37 -0400

    Create firered_overrides.h

---
+++ Sample Project/src/firered_overrides.h
@@ -0,0 +1,5 @@
+/*
+Override function headers here. Example to override "returnsAnIntAndTakesTwoShorts":
+#define firered_returnsAnIntAndTakesTwoShorts_RETURN_VALUE int
+#define firered_returnsAnIntAndTakesTwoShorts_ARGUMENT_LIST short, short
+*/
